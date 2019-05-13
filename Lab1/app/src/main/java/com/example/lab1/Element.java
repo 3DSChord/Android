2021f6bbbbf8d55008673486d6c12c145580b92b
3DSChord <31824652+3DSChord@users.noreply.github.com>
2019-05-13 23:36:01 +0300

    First commit of Lab1.

---
+++ Lab1/app/src/main/java/com/example/lab1/Element.java
@@ -0,0 +1,98 @@
+package com.example.lab1;
+
+    public class Element {
+        /** String array of words for tens Names */
+        static final String[] tensNames =
+                {
+                        "", "", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY",
+                        "EIGHTY", "NINETY"
+                };
+
+        /** String array of words for ones Names */
+        static final String[] onesNames =
+                {
+                        "", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+                        "NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN",
+                        "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN"
+                };
+        private int val;
+
+        public Element(int val) {
+            this.val = val;
+        }
+
+        public int getVal() { return this.val;}
+        public String getStr() {return evaluate(this.val);}
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        private String evaluate(long number)
+        {
+            long temp = number;
+
+            long crore = temp / 10000000;
+            temp %= 10000000;
+
+            long lakh = temp / 100000;
+            temp %= 100000;
+
+            long thousands = temp / 1000;
+            temp %= 1000;
+
+            long hundreds = temp / 100;
+            temp %= 100;
+
+            StringBuffer result = new StringBuffer(30);
+
+            if (crore > 0)
+            {
+                result.append(evaluate(crore) + " CRORE ");
+            }
+
+            if (lakh > 0)
+            {
+                result.append(evaluate(lakh) + " LAKH ");
+            }
+
+            if (thousands > 0)
+            {
+                result.append(evaluate(thousands) + " THOUSAND ");
+            }
+
+            if (hundreds > 0)
+            {
+                result.append(evaluate(hundreds) + " HUNDRED ");
+            }
+
+            if (temp != 0)
+            {
+                if (number >= 100)
+                {
+                    result.append("AND ");
+                }
+
+                if ((0 < temp) && (temp <= 19))
+                {
+                    result.append(onesNames[( int ) temp]);
+                }
+                else
+                {
+                    long tens = temp / 10;
+                    long ones = temp % 10;
+                    result.append(tensNames[( int ) tens] + " ");
+                    result.append(onesNames[( int ) ones]);
+                }
+            }
+
+            if ((result
+                    .toString()).trim()
+                    .equals(""))
+            {
+                result.append(" ZERO ");
+            }
+
+            return result.toString();
+        }
+    }
+
